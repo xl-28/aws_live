d22995dbaa63125aa7bab6eda0f0ac942a40d236
xl-28 <167161602+xl-28@users.noreply.github.com>
2024-04-16 12:59:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxc8cs8gspt4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "lohxinling-employee"
+customregion = "us-east-1"
+
